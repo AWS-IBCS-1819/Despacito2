c4129d033dc91eff9310d7a4b03f4a8dd7cd414a
Shubing Sun <Shubing_Sun@aw.org>
2018-09-12 13:07:13 -0700

    g

--- AboutMe.java
+++ AboutMe.java
@@ -5,23 +5,25 @@ import java.util.*;
 
 public class AboutMe{
 
-  public static String Dinosaur; // creation things
+  public static String dinosaur; // creation things
   public static boolean TRex;
   public static String OTP;
   public static String House;
-  public static String Patronus;
+  public static String patronus;
   public static boolean Dorm;
   public static String Rmate;
   public static String Dgroup;
   public static String Food;
   public static boolean Pet;
+  public static String name;
 
   public AboutMe() {
-    String Dinosaur = "ankylosaurus";
+    String name = "Emilie";
+    String dinosaur = "ankylosaurus";
     boolean TRex = false;
     String OTP = "bubblegum/marceline";
     String House = "Hufflepuff";
-    String Patronus = "lizard";
+    String patronus = "lizard";
     boolean Dorm = false;
     String Rmate = "";
     String Dgroup = "";
@@ -31,7 +33,7 @@ public class AboutMe{
 
 public static void main(String[] args) {
 
-  AboutMe Dis = new AboutMe(); // use the method
+  AboutMe tis = new AboutMe(); // use the method
 
   Scanner sc = new Scanner(System.in);
 
@@ -39,30 +41,26 @@ public static void main(String[] args) {
   System.out.println("We have collect info of a certain individual");
   System.out.println("Please input the things about the person you would like to know");
 
-  System.out.println("favrite dinosaur:" + Dis.dinosaur +"/n" + "name:" + Dis.name + "/n" + "Hogwarts house:" + Dis.House);
-  System.out.println("patronus:" + Dis.patronus);
+  System.out.print("favrite dinosaur:" + tis.dinosaur +"\n" + "name:" + tis.name + "\n" + "Hogwarts house:" + tis.House + "\n");
+  System.out.println("patronus:" + tis.patronus);
 
-
-  if(Dis.Dorm == true){
-    System.out.println("Dorm group:" + Dis.Dgroup + "/n" + "Roommate" + Dis.Rmate);
-  }
-
-  if(Dis.Pet == true){
-    System.out.println(Dis.name + " has a pet");
-  }
-
-  if(Dis.TRex == true){
-    System.out.println(Dis.name + " only knows tyrannosaurus rex");
-  }
-
-  if(sc.nextLine() = "413612914"){
+  if(sc.nextLine() == "413612914"){
     System.out.println("https://www.homestuck.com/story");// nice easter egg
   }
 
+  sc.close();
 
+  if(tis.Dorm == true){
+    System.out.println("Dorm group:" + tis.Dgroup + "\n" + "Roommate" + tis.Rmate);
+  }
 
+  if(tis.Pet == true){
+    System.out.println(tis.name + " has a pet");
+  }
 
-
+  if(tis.TRex == true){
+    System.out.println(tis.name + " only knows tyrannosaurus rex");
+  }
 
 }
 
