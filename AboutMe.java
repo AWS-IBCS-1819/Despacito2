244fde17b29f89b46e7e56557494dcda92af03a7
Shubing Sun <Shubing_Sun@aw.org>
2018-09-12 13:50:05 -0700

    N

--- AboutMe.java
+++ AboutMe.java
@@ -41,14 +41,12 @@ public static void main(String[] args) {
   System.out.println("We have collect info of a certain individual");
   System.out.println("Please input the things about the person you would like to know");
 
-  System.out.print("favrite dinosaur:" + tis.dinosaur +"\n" + "name:" + tis.name + "\n" + "Hogwarts house:" + tis.House + "\n");
+  System.out.println("favrite dinosaur:" + tis.dinosaur +"\n" + "name:" + tis.name + "\n" + "Hogwarts house:" + tis.House);
   System.out.println("patronus:" + tis.patronus);
 
   if(sc.nextLine() == "413612914"){
     System.out.println("https://www.homestuck.com/story");// nice easter egg
   }
-
-  sc.close();
 
   if(tis.Dorm == true){
     System.out.println("Dorm group:" + tis.Dgroup + "\n" + "Roommate" + tis.Rmate);
@@ -61,6 +59,82 @@ public static void main(String[] args) {
   if(tis.TRex == true){
     System.out.println(tis.name + " only knows tyrannosaurus rex");
   }
+
+  /*int error = 0;
+  String[] Areo;
+  Areo = new String[11];
+  System.out.println("_ _ _ _ _ _ _ _ _ _ _");
+  System.out.println("guess the word: (letter by letter)");
+  if(sc.nextLine == "p"){
+    Areo[0] = "p";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "t"){
+    Areo[1] = "t";
+    Areo[8] = "t";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "e"){
+    Areo[2] = "e";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "r"){
+    Areo[3] = "r";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "o"){
+    Areo[4] = "o";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "d"){
+    Areo[5] = "d";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "a"){
+    Areo[6] = "a";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "c"){
+    Areo[7] = "c";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "y"){
+    Areo[9] = "y";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else if(sc.nextLine == "l"){
+    Areo[10] = "l";
+    if(Arrays.toString(Areo) == "pterodactyl"){
+      break;
+    }
+  }
+  else{
+    error ++;
+    if (error > 6){
+      System.out.println("Game Over");
+      break;
+    }
+  }*/
+
+
 
 }
 
