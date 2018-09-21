79bcae458a310bb615dc7fef67fe10385e5f0a75
Shubing Sun <Shubing_Sun@aw.org>
2018-09-21 13:50:45 -0700

    k

--- Gradebook.java
+++ Gradebook.java
@@ -4,36 +4,31 @@ import java.io.*;
 
 public class Gradebook{
 
-  ArrayList<Double[]> list = new ArrayList<>();
+  public void readFile(String filename) {
 
-  public void readFile(String filename){
-    try{
-      FileReader fr = new FileReader(filename);
-      BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-      while((line = bufferedReader.readLine()) != null) {
-                line.convertInt();
+    StringBuilder myStr = new StringBuilder();
+      try {
+        File randtxt = new File("grades.txt");
+        Scanner sc = new Scanner(randtxt);
+        while (sc.hasNext()){
+          myStr.append(sc.nextLine() + "\n");
+        }
+    }
+      catch (FileNotFoundException e){
+          e.printStackTrace();
               }
 
-      bufferedReader.close();
-
-    }
-    catch(FileNotFoundException ex){
-      System.out.println("Unable to open file '" + fileName + "'");
-    }
-    catch(IOException ex) {
-            System.out.println("Error reading file '"+ fileName + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
-        }
+      System.out.println(myStr);
 
   }
 
-  public double convertInt(String s){
 
-  }
+
 
   public static void main(String[] args) {
+
+    Gradebook gb = new Gradebook();
+    gb.readFile("grades.txt");
 
   }
 
