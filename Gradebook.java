5ceff03c20cb4cc19730883596f002280dff0536
Shubing Sun <Shubing_Sun@aw.org>
2018-09-26 13:40:21 -0700

    f

--- Gradebook.java
+++ Gradebook.java
@@ -4,32 +4,44 @@ import java.io.*;
 
 public class Gradebook{
 
-  public void readFile(String filename) {
-
-    StringBuilder myStr = new StringBuilder();
-      try {
-        File randtxt = new File("grades.txt");
-        Scanner sc = new Scanner(randtxt);
-        while (sc.hasNext()){
-          myStr.append(sc.nextLine() + "\n");
-        }
-    }
-      catch (FileNotFoundException e){
-          e.printStackTrace();
-              }
-
-      System.out.println(myStr);
+  public void findMean(String mean){//finds average of grades
 
   }
 
+  public void findSpread(String spread){//finds number of As/Bs/Cs/Ds/Fs
+
+  }
+
+  public void highestGrade(String highest){
+    System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
+  }
+
+  public void lowestGrade(String lowest){
+    System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
+  }
+
+  public void roster(String roseter){//finds name of all students
+
+  }
 
 
 
   public static void main(String[] args) {
 
-    Gradebook gb = new Gradebook();
-    gb.readFile("grades.txt");
+   Gradebook gb = new Gradebook();
 
-  }
+   StringBuilder myStr = new StringBuilder();
+     try {
+       File randtxt = new File("grades.txt");
+       Scanner sc = new Scanner(randtxt);
+       myStr.append(sc.nextLine());
+       System.out.println(myStr);
+   }
+     catch (FileNotFoundException e){
+         e.printStackTrace();
+             }
+
+
+}
 
 }
