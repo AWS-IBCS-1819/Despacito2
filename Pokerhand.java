34e2be558f00f2d580136dcf6a6b1134b68323d5
Shubing Sun <Shubing_Sun@aw.org>
2018-10-05 13:44:13 -0700

    e

---
+++ Pokerhand.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Pokerhand{
+  public static void main(String[] args) {
+    Deck uno = new Deck();
+    System.out.println(uno.cards);
+    System.out.println(uno.cards.size());
+  }
+}
