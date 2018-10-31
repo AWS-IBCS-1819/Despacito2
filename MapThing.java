cf8fd8f8205dc506a7c64f230db6e985f346c658
Shubing Sun <Shubing_Sun@aw.org>
2018-10-31 13:45:40 -0700

    ligma

---
+++ MapThing.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class MapThing{
+
+  public static void main(String[] args) {
+    Map<String,String> m = new HashMap<String,String>();
+    m.put("why","pickle rick");
+    m.put("Heck","Sans");
+    m.put("LOL","Naruto");
+    System.out.println(m);
+
+  }
+}
